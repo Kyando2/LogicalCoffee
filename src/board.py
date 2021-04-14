9852564784f98e6f2e567472ecb8d407fce0e45b
Kyando_2 <amehikoji@gmail.com>
2021-04-14 18:24:50 -0400

    Added all pieces

--- src/board.py
+++ src/board.py
@@ -4,27 +4,30 @@ from pieces import Rook, Knight, Bishop, Queen, King, Pawn
 
 import time
 
+
 class Board:
     def __init__(self, display):
-        self.pieces = {i: {j: None for j in range(8)} for i in range(8)} # ROW (10 ;; 0-9) : COLUMN (8 ;; 0-7)
+        self.pieces = {i: {j: None for j in range(8)} for i in range(8)}  # ROW (10 ;; 0-9) : COLUMN (8 ;; 0-7)
         self.pieces_linear = []
         self.screen = display
         self.selected = None
-        self.ys = pygame.transform.scale(pygame.image.load("../images/ys.png"), (60,60))
-        self.background = pygame.transform.scale(pygame.image.load("../images/cb.png"), (800,800))
+        self.ys = pygame.transform.scale(pygame.image.load("../images/ys.png"), (20, 20))
+        self.background = pygame.transform.scale(pygame.image.load("../images/cb.png"), (800, 800))
         self.initialize_position()
         self.to_blit = []
+        self.dopplegangers = []
         self.turn = 1
 
     def initialize_position(self):
         # Creates pieces of specified type `cls` for both teams on the provided Iterator of tuples `st`
         def create_piece(st, cls):
             for coord in st:
-                x, y = coord[0], coord[1] # Unpack coord
+                x, y = coord[0], coord[1]  # Unpack coord
                 self.pieces[x][y] = cls(1, (x, y))
                 self.pieces_linear.append(self.pieces[x][y])
-                self.pieces[x][(7-y)] = cls(2, (x, (7-y)))
-                self.pieces_linear.append(self.pieces[x][(7-y)])
+                self.pieces[x][(7 - y)] = cls(2, (x, (7 - y)))
+                self.pieces_linear.append(self.pieces[x][(7 - y)])
+
         # Create all the pieces
         create_piece(((0, 0), (7, 0)), Rook)
         create_piece(((1, 0), (6, 0)), Knight)
@@ -34,7 +37,7 @@ class Board:
         create_piece([(x, 1) for x in range(8)], Pawn)
 
     def update(self):
-        self.screen.blit(self.background, (0,0))
+        self.screen.blit(self.background, (0, 0))
         for piece in self.pieces_linear:
             # Blit the piece's image on the screen
             self.screen.blit(piece.surf, find_screen_location_from_position(piece))
@@ -42,7 +45,8 @@ class Board:
             self.screen.blit(item[0], item[1])
 
     def get_piece_from_mouse_position(self, pos):
-        pos = (int((str(pos[0])[0] if len(list(str(pos[0])))==3 else "0")), 7-int((str(pos[1])[0] if len(list(str(pos[1])))==3 else "0")))
+        pos = (int((str(pos[0])[0] if len(list(str(pos[0]))) == 3 else "0")),
+               7 - int((str(pos[1])[0] if len(list(str(pos[1]))) == 3 else "0")))
         if (found_piece := self.pieces[pos[0]][pos[1]]) is not None:
             return found_piece
         return None
@@ -50,27 +54,48 @@ class Board:
     def select(self, pos):
         self.to_blit.clear()
         selected_piece = self.get_piece_from_mouse_position(pos)
-        print(selected_piece)
         if selected_piece:
             if selected_piece.team == self.turn:
                 self.selected = selected_piece
                 # Show possible moves
                 for pos in selected_piece.list_possible_moves(board=self):
-                    location = (find_screen_location_from_position(coords=pos)[0]+20, find_screen_location_from_position(coords=pos)[1]+20)
+                    location = (find_screen_location_from_position(coords=pos)[0] + 40,
+                                find_screen_location_from_position(coords=pos)[1] + 40)
                     self.to_blit.append((self.ys, location))
+            else:
+                if self.selected:
+                    if self.selected.team == self.turn:
+                        if (x := [int((str(pos[0])[0] if len(list(str(pos[0]))) == 3 else "0")), 7 - int((str(pos[1])[
+                            0] if len(list(str(pos[1]))) == 3 else "0"))]) in self.selected.list_possible_moves(self):
+                            old_pos = self.selected.pos
+                            self.selected.pos = tuple(x)
+                            self.pieces[old_pos[0]][old_pos[1]] = None
+                            selected_piece.destroy_connected(self, self.selected)
+                            try:
+                                self.pieces_linear.remove(selected_piece)
+                            except:
+                                pass
+                            self.pieces[self.selected.pos[0]][self.selected.pos[1]] = self.selected
+                            self.switch_turn()
         else:
             if self.selected:
                 if self.selected.team == self.turn:
-                    if self.selected.is_legal_move((int((str(pos[0])[0] if len(list(str(pos[0])))==3 else "0")), 7-int((str(pos[1])[0] if len(list(str(pos[1])))==3 else "0"))), self):
+                    if (x := [int((str(pos[0])[0] if len(list(str(pos[0]))) == 3 else "0")), 7 - int((str(pos[1])[
+                    0] if len(list(str(pos[1]))) == 3 else "0"))]) in self.selected.list_possible_moves(self):
+                        if self.selected.__class__ == Pawn:
+                            self.selected.spawn_doppleganger(tuple(x), self)
                         old_pos = self.selected.pos
-                        self.selected.pos = self.selected.getis_legal_move((int((str(pos[0])[0] if len(list(str(pos[0])))==3 else "0")), 7-int((str(pos[1])[0] if len(list(str(pos[1])))==3 else "0"))), self)
+                        self.selected.pos = tuple(x)
                         self.pieces[old_pos[0]][old_pos[1]] = None
                         self.pieces[self.selected.pos[0]][self.selected.pos[1]] = self.selected
                         self.switch_turn()
 
-
     def switch_turn(self):
-        self.turn = 3-self.turn
+        for doppleganger in self.dopplegangers:
+            if doppleganger.team != self.turn:
+                doppleganger.explode(self)
+                self.dopplegangers.remove(doppleganger)
+        self.turn = 3 - self.turn
 
 
 def find_screen_location_from_position(piece=None, coords=None):
@@ -78,5 +103,5 @@ def find_screen_location_from_position(piece=None, coords=None):
         local_pos = coords
     else:
         local_pos = piece.pos
-    pub_pos = (local_pos[0], 7-local_pos[1])
-    return (pub_pos[0]*100, pub_pos[1]*100)
+    pub_pos = (local_pos[0], 7 - local_pos[1])
+    return (pub_pos[0] * 100, pub_pos[1] * 100)
--- src/pieces.py
+++ src/pieces.py
@@ -21,9 +21,16 @@ class Piece:
                 if pos[0]>7:
                     pos = [pos[0] - 8, pos[1]]
                 if board.pieces[pos[0]][pos[1]] is not None:
-                    continue
+                    if board.pieces[pos[0]][pos[1]].team == board.turn:
+                        continue
                 moves.append(pos)
         return moves
+
+    def explode(self, board):
+        return
+
+    def destroy_connected(self, board, originator=None):
+        pass
 
 
 class Bishop(Piece):
@@ -31,36 +38,51 @@ class Bishop(Piece):
     filename_black = "bd.png"
     filename_white = "bl.png"
 
+    def list_possible_moves(self, board):
+        multiboard_pos = (self.pos[0], self.pos[1])
+        diagonals = [(1,1), (-1,-1), (-1, 1), (1, -1)]
+        moves = []
+        for vector in diagonals:
+            clcting = True
+            i = 1
+            while clcting:
+                calculating_position = ((i*vector[0])+multiboard_pos[0], (i*vector[1])+multiboard_pos[1])
+                if calculating_position[0]<-7 or calculating_position[0]>15 or calculating_position[1]<0 or calculating_position[1]>7:
+                    clcting = False
+                    continue
+                if calculating_position[0]<0:
+                    posx = [calculating_position[0]+8, calculating_position[1]]
+                elif calculating_position[0]>7:
+                    posx = [calculating_position[0] - 8, calculating_position[1]]
+                else:
+                    posx = [calculating_position[0], calculating_position[1]]
+                if (that_piece:=board.pieces[posx[0]][posx[1]]):
+                    if that_piece.team != self.team:
+                       moves.append(posx)
+                    clcting = False
+                    continue
+                moves.append(posx)
+                i+=1
+            print('--')
+
+        return moves
+
 
 class Knight(Piece):
 
     filename_black = "nd.png"
     filename_white = "nl.png"
 
-    def is_legal_move(self, pos, board):
+    def is_legal_move(self, pos, board, get=False):
         try:
             if board.pieces[pos[0]][pos[1]] is not None:
-                return False
+                if board.pieces[pos[0]][pos[1]].team == board.turn:
+                    return False
         except:
             pass
         move_vectors = [(2*i, 1*j) for j in [-1, 1] for i in [-1, 1]]
         move_vectors.extend((1*i, 2*j) for j in [-1, 1] for i in [-1, 1])
         for move_vector in move_vectors:
-            print(move_vector, pos, (pos[0]+move_vector[0], pos[1]+move_vector[1]), self.pos)
-            if (x := (pos[0]+move_vector[0], pos[1]+move_vector[1])) == self.pos:
-                return True
-        return False
-
-    def getis_legal_move(self, pos, board):
-        try:
-            if board.pieces[pos[0]][pos[1]] is not None:
-                return False
-        except:
-            pass
-        move_vectors = [(2*i, 1*j) for j in [-1, 1] for i in [-1, 1]]
-        move_vectors.extend((1*i, 2*j) for j in [-1, 1] for i in [-1, 1])
-        for move_vector in move_vectors:
-            # print(move_vector, pos, (pos[0]+move_vector[0], pos[1]+move_vector[1]), self.pos)
             if (x := (pos[0]+move_vector[0], pos[1]+move_vector[1])) == self.pos:
                 posx = [pos[0], pos[1]]
                 if pos[0]<0:
@@ -68,8 +90,11 @@ class Knight(Piece):
                 if pos[0]>7:
                     posx = [pos[0] - 8, pos[1]]
                 if board.pieces[posx[0]][posx[1]] is not None:
-                    return False
-                return pos
+                    if board.pieces[posx[0]][posx[1]].team == board.turn:
+                        return False
+                if get:
+                    return pos
+                return True
         return False
 
 
@@ -78,20 +103,184 @@ class Rook(Piece):
     filename_black = "rd.png"
     filename_white = "rl.png"
 
+    def list_possible_moves(self, board):
+        multiboard_pos = (self.pos[0], self.pos[1])
+        lines = [(1,0), (-1,0), (0, 1), (0, -1)]
+        moves = []
+        for vector in lines:
+            clcting = True
+            i = 1
+            while clcting:
+                calculating_position = ((i*vector[0])+multiboard_pos[0], (i*vector[1])+multiboard_pos[1])
+                if calculating_position[0]<-7 or calculating_position[0]>15 or calculating_position[1]<0 or calculating_position[1]>7:
+                    clcting = False
+                    continue
+                if calculating_position[0]<0:
+                    posx = [calculating_position[0]+8, calculating_position[1]]
+                elif calculating_position[0]>7:
+                    posx = [calculating_position[0] - 8, calculating_position[1]]
+                else:
+                    posx = [calculating_position[0], calculating_position[1]]
+                if (that_piece:=board.pieces[posx[0]][posx[1]]):
+                    if that_piece.team != self.team:
+                        moves.append(posx)
+                    clcting = False
+                    continue
+                moves.append(posx)
+                i+=1
+        return moves
 
 class King(Piece):
 
     filename_black = "kd.png"
     filename_white = "kl.png"
 
+    def list_possible_moves(self, board):
+        multiboard_pos = (self.pos[0], self.pos[1])
+        diagonals = [(1,1), (-1,-1), (-1, 1), (1, -1), (1,0), (-1,0), (0, 1), (0, -1)]
+        moves = []
+        for vector in diagonals:
+            clcting = True
+            i = 1
+            while clcting:
+                calculating_position = ((i*vector[0])+multiboard_pos[0], (i*vector[1])+multiboard_pos[1])
+                if calculating_position[0]<-7 or calculating_position[0]>15 or calculating_position[1]<0 or calculating_position[1]>7:
+                    clcting = False
+                    continue
+                if calculating_position[0]<0:
+                    posx = [calculating_position[0]+8, calculating_position[1]]
+                elif calculating_position[0]>7:
+                    posx = [calculating_position[0] - 8, calculating_position[1]]
+                else:
+                    posx = [calculating_position[0], calculating_position[1]]
+                if (that_piece:=board.pieces[posx[0]][posx[1]]):
+                    if that_piece.team != self.team:
+                        if i<2:
+                            moves.append(posx)
+                    clcting = False
+                    continue
+                if i<2:
+                    moves.append(posx)
+                i+=1
+        return moves
 
 class Queen(Piece):
 
     filename_black = "qd.png"
     filename_white = "ql.png"
 
+    def list_possible_moves(self, board):
+        multiboard_pos = (self.pos[0], self.pos[1])
+        diagonals = [(1,1), (-1,-1), (-1, 1), (1, -1), (1,0), (-1,0), (0, 1), (0, -1)]
+        moves = []
+        for vector in diagonals:
+            clcting = True
+            i = 1
+            while clcting:
+                calculating_position = ((i*vector[0])+multiboard_pos[0], (i*vector[1])+multiboard_pos[1])
+                if calculating_position[0]<-7 or calculating_position[0]>15 or calculating_position[1]<0 or calculating_position[1]>7:
+                    clcting = False
+                    continue
+                if calculating_position[0]<0:
+                    posx = [calculating_position[0]+8, calculating_position[1]]
+                elif calculating_position[0]>7:
+                    posx = [calculating_position[0] - 8, calculating_position[1]]
+                else:
+                    posx = [calculating_position[0], calculating_position[1]]
+                if (that_piece := board.pieces[posx[0]][posx[1]]):
+                    if that_piece.team != self.team:
+                        moves.append(posx)
+                    clcting = False
+                    continue
+                moves.append(posx)
+                i+=1
+        return moves
 
 class Pawn(Piece):
 
     filename_black = "pd.png"
     filename_white = "pl.png"
+
+    def is_legal_move(self, pos, board, get=False):
+        capture = False
+        # forward = -1 if self.team == 2 else 1
+        try:
+            if board.pieces[pos[0]][pos[1]] is not None:
+                if board.pieces[pos[0]][pos[1]].team == board.turn:
+                    return False
+                else:
+                    capture = True
+        except:
+            pass
+        posx = [pos[0], pos[1]]
+        if pos[0]<0:
+            posx = [pos[0]+8, pos[1]]
+        if pos[0]>7:
+            posx = [pos[0] - 8, pos[1]]
+        if board.pieces[posx[0]][posx[1]] is not None:
+            if board.pieces[posx[0]][posx[1]].team == board.turn:
+                return False
+            else:
+                capture = True
+
+        x, y = pos[0], pos[1]
+        my_x, my_y = self.pos[0], self.pos[1]
+        diff_x, diff_y = x-my_x, (y-my_y)
+        if self.team == 1:
+            if diff_y < 1: return False
+            if diff_x != 0 and capture==False: return False
+            if diff_y>1 and capture==True: return False
+            if diff_x > 1 or diff_x < -1: return False
+            if diff_y>2: return False
+            if diff_y>1 and my_y != 1: return False
+        elif self.team == 2:
+            if diff_y > -1: return False
+            if diff_x != 0 and capture==False: return False
+            if diff_x > 1 or diff_x < -1: return False
+            if diff_y<-1 and capture==True: return False
+            if diff_y<-2: return False
+            if diff_y<-1 and my_y != 6: return False
+
+        if diff_x == 0:
+            if self.team == 1:
+                if board.pieces[(self.pos[0])][self.pos[1]+1] is not None or (board.pieces[(self.pos[0])][self.pos[1]+2] is not None and diff_y>1):
+                    return False
+
+            if self.team == 2:
+                if board.pieces[(self.pos[0])][self.pos[1]-1] is not None or (board.pieces[(self.pos[0])][self.pos[1]-2] is not None and diff_y<-1):
+                    return False
+
+        posx = [x, y]
+        if pos[0]<0:
+            posx = [pos[0]+8, pos[1]]
+        if pos[0]>7:
+            posx = [pos[0] - 8, pos[1]]
+        if board.pieces[posx[0]][posx[1]] is not None:
+            if board.pieces[posx[0]][posx[1]].team == board.turn:
+                return False
+        return True
+
+    def spawn_doppleganger(self, pos, board):
+        x, y = pos[0], pos[1]
+        my_x, my_y = self.pos[0], self.pos[1]
+        diff_x, diff_y = x-my_x, (y-my_y)
+        if diff_x == 0:
+            if diff_y == 2 or diff_y == -2:
+                board.pieces[x][y+(-1 if diff_y == 2 else 1)] = Doppleganger(self.team, self, (x, y+(-1 if diff_y == 2 else 1)))
+                board.dopplegangers.append(board.pieces[x][y+(-1 if diff_y == 2 else 1)])
+
+
+
+class Doppleganger:
+    def __init__(self, team, connected: Piece, pos):
+        self.team = team
+        self.pos = pos
+        self.connected = connected
+
+    def explode(self, board):
+        board.pieces[self.pos[0]][self.pos[1]] = None
+
+    def destroy_connected(self, board, originator):
+        if originator.__class__ == Pawn:
+            board.pieces[self.connected.pos[0]][self.connected.pos[1]] = None
+            board.pieces_linear.remove(self.connected)
